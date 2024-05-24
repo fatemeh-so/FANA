f5fbcbbb477643c5b0291129be7f05d33fed63ff
Fatemeh Soleimani <fwtemeh.soleimanii@gmail.com>
2024-05-24 01:29:00 -0800

    artist icon size changed

--- src/features/artist/SingerIcons.jsx
+++ src/features/artist/SingerIcons.jsx
@@ -12,11 +12,11 @@ function SingerIcons({ singer }) {
       {' '}
       <h1 className='my-4 text-[2rem]  '>Artist</h1>
       <div className='flex justify-center'>
-        <div className=' grid grid-cols-3 gap-[3rem]  lg:gap-[3rem] md:grid-cols-4 xl:grid-cols-5 py-[2rem]'>
+        <div className=' grid grid-cols-3 gap-[1rem]  lg:gap-[3rem] md:grid-cols-4 xl:grid-cols-5 py-[2rem]'>
           {singer?.map((singer, index) => (
             <div
               key={index}
-              className='relative h-[10rem] w-[10rem] xl:h-[20rem] xl:w-[20rem] '
+              className='relative h-[8rem] w-[8rem] md:h-[11rem] md:w-[11rem] xl:h-[18rem] xl:w-[18rem] '
             >
               <img
                 src={singer.image}
@@ -28,7 +28,7 @@ function SingerIcons({ singer }) {
               />
               <div className='text-center absolute top-[9.6rem] xl:top-[20rem] 5 rounded-b-[2rem] bottom-0 left-0 w-full bg-opacity-30 text-white p-2'>
                 <p className=' pl-2 text-sm md:text-lg xl:text-xl font-semibold '>
-                  {singer.nickName}
+                  {/* {singer.nickName} */}
                 </p>
               </div>
             </div>
